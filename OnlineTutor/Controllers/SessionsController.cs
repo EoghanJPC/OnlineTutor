f91daabd9d8f71712200be486edc39242c9208d2
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 22:28:13 +0100

    Feat(DTO) = Session DTO and Controller Created

---
+++ OnlineTutor/Controllers/SessionsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineTutor.Data;
+using OnlineTutor.Models;
+
+namespace OnlineTutor.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sessions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sessions.Include(s => s.Tutor);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Sessions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Tutor)
+                .FirstOrDefaultAsync(m => m.SessionId == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // GET: Sessions/Create
+        public IActionResult Create()
+        {
+            ViewData["TutorId"] = new SelectList(_context.Tutors, "TutorId", "TutorName");
+            return View();
+        }
+
+        // POST: Sessions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SessionId,SessionTime,TutorId")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TutorId"] = new SelectList(_context.Tutors, "TutorId", "TutorName", session.TutorId);
+            return View(session);
+        }
+
+        // GET: Sessions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            ViewData["TutorId"] = new SelectList(_context.Tutors, "TutorId", "TutorName", session.TutorId);
+            return View(session);
+        }
+
+        // POST: Sessions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SessionId,SessionTime,TutorId")] Session session)
+        {
+            if (id != session.SessionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(session);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.SessionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TutorId"] = new SelectList(_context.Tutors, "TutorId", "TutorName", session.TutorId);
+            return View(session);
+        }
+
+        // GET: Sessions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Tutor)
+                .FirstOrDefaultAsync(m => m.SessionId == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // POST: Sessions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.SessionId == id);
+        }
+    }
+}
---
+++ OnlineTutor/DTOs/SessionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineTutor.DTOs
+{
+	public class SessionDTO
+	{
+		public int SessionId { get; set; }
+
+		public string FormattedTime { get; set; } = default!;
+		public string MeetingLink { get; set; } = default!;
+
+		public string TutorName { get; set; } = default!;
+		public string SubjectName { get; set; } = default!;
+
+		public bool UpcomingSession => DateTime.Parse(FormattedTime) > DateTime.Now;
+	}
+}
