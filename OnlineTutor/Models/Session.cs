e521b602c1e13ba6152272db8fee608c336f2f42
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 20:35:22 +0100

    Feat(Model) - Session Model Created

---
+++ OnlineTutor/Models/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineTutor.Models
+{
+	public class Session
+	{
+		public int SessionId { get; set; }
+
+		public DateTime SessionTime { get; set; }
+
+		public int TutorId { get; set; }
+
+		public Tutor Tutor { get; set; }
+	}
+}
