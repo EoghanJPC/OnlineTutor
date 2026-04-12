8834557028df1eb97d694aa10a07e5b9e8fe8411
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 20:24:27 +0100

    Add project files.

---
+++ OnlineTutor/Models/Subject.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineTutor.Models
+{
+	public class Subject
+	{
+		public int SubjectId { get; set; }
+
+		public string SubjectName { get; set; }
+
+		public string SubjectDesc { get; set; }
+
+		public ICollection<Tutor> Tutors { get; set; }
+	}
+}
