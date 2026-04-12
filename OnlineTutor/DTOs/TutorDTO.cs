056296603df71cdf1402dc0a71b178730feeaa62
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 22:06:37 +0100

    Feat(DTOs) - Tutor DTO and Controller Created for FrontEnd

---
+++ OnlineTutor/DTOs/TutorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineTutor.DTOs
+{
+	public class TutorDTO
+	{
+		public int TutorId { get; set; }
+		public string TutorName { get; set; } = default!;
+		public string SubjectName { get; set; } = default!;
+	}
+}
