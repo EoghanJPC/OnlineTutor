ff48598d2837fdc369c978dc17232c43c377118e
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 21:54:11 +0100

    Feat(DTOs) - Subjects Controller and DTO created for Subjects Front End

---
+++ OnlineTutor/DTOs/SubjectDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineTutor.DTOs
+{
+	public class SubjectDTO
+	{
+		public int SubjectId { get; set; }
+		public string SubjectName { get; set; } = default!;
+		public string Description { get; set; } = default!; 
+	}
+}
---
+++ OnlineTutor/Data/Migrations/20260412200141_AddedRequiredFields.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineTutor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRequiredFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubjectName",
+                table: "Tutors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubjectName",
+                table: "Tutors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
