fcee4f1c9f6d02bc1a2b999254acae3605589439
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 20:37:07 +0100

    Feat(Database) - Database Connection to Models Created

--- OnlineTutor/Data/ApplicationDbContext.cs
+++ OnlineTutor/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineTutor.Models;
 
 namespace OnlineTutor.Data
 {
 	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 	{
+		public DbSet<Subject> Subjects { get; set; }
+		public DbSet<Tutor> Tutors { get; set; }
+		public DbSet<Session> Sessions { get; set; }
 	}
 }
