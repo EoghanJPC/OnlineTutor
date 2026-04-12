7a3b7735ed541ac1ed61fd3212cd179b8cfe1961
EoghanJPC <eoghanjpc@gmail.com>
2026-04-12 20:28:11 +0100

    Feat(Models) - Tutor Model Created

---
+++ OnlineTutor/Models/Tutor.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineTutor.Models
+{
+	public class Tutor
+	{
+		public int TutorId { get; set; }
+
+		public string TutorName { get; set; }
+
+		public int SubjectId { get; set; }
+
+		public string SubjectName { get; set; }
+
+		public ICollection<Session> Sessions { get; set; }
+	}
+}
