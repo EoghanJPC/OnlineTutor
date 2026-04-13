2ec6f6dd2acf7c7f50cb1af184e953fe129634e4
EoghanJPC <eoghanjpc@gmail.com>
2026-04-13 11:53:54 +0100

    Feat(Testing) = Subject Testing

---
+++ OnlineTutor.Tests/SubjectDTOTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineTutor.DTOs;
+
+namespace OnlineTutor.Tests
+{
+	public class SubjectDTOTests
+	{
+		[Fact]
+		public void CorrectMapping()
+		{
+			var dto = new SubjectDTO
+			{
+				SubjectName = "DTO Testing",
+				Description = "The Incredible Testing of Impecable DTOs",
+			};
+
+			Assert.Equal("DTO Testing", dto.SubjectName);
+			Assert.Equal("The Incredible Testing of Impecable DTOs", dto.Description);
+		}
+	}
+}
