7ba8bcc29deefb044aec38f533d1d4881155b615
EoghanJPC <eoghanjpc@gmail.com>
2026-04-13 12:20:35 +0100

    Feat(Testing) = Model Testing

---
+++ OnlineTutor.Tests/ModelTesting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using OnlineTutor.Models;
+
+namespace OnlineTutor.Tests
+{
+	public class ModelTesting
+	{
+		[Fact]
+		public void TutorMissingName()
+		{
+			var tutor = new Tutor { TutorName = "" };
+			var valContext = new ValidationContext(tutor);
+			var results = new List<ValidationResult>();
+
+			var validationCheck = Validator.TryValidateObject(tutor, valContext, results, true);
+
+			Assert.False(validationCheck);
+		}
+
+		[Fact]
+		public void SessionMissingLink()
+		{
+			var session = new Session { MeetingLink = "" };
+			var valContext = new ValidationContext(session);
+			var results = new List<ValidationResult>();
+
+			var validationCheck = Validator.TryValidateObject(session, valContext, results, true);
+
+			Assert.False(validationCheck);
+		}
+	}
+}
