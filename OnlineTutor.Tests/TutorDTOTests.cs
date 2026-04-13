610e5d8c31d030b64bc0436ddf164557a8a72d7f
EoghanJPC <eoghanjpc@gmail.com>
2026-04-13 11:51:24 +0100

    Feat(Testing) = Tutor Model Testing

---
+++ OnlineTutor.Tests/TutorDTOTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineTutor.DTOs;
+
+namespace OnlineTutor.Tests
+{
+	public class TutorDTOTests
+	{
+		[Fact]
+		public void CorrectMapping()
+		{
+			var dto = new TutorDTO
+			{
+				TutorName = "J Bloggs",
+				SubjectName = "DTO Testing",
+			};
+
+			Assert.Equal("J Bloggs", dto.TutorName);
+			Assert.Equal("DTO Testing", dto.SubjectName);
+		}
+	}
+}
