ff4abf2d0b16c4dad89081bce47c79de2b0ee7ad
EoghanJPC <eoghanjpc@gmail.com>
2026-04-27 02:24:43 +0100

    Feat(Data) - Class created for seeding admin user

---
+++ OnlineTutor/Data/DataInitaliser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineTutor.Data
+{
+	public class DataInitaliser
+	{
+		public static async Task SeedRoles(IServiceProvider serviceProvider) {
+			var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+			var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+			string[] roles = { "Admin", "Registered" };
+			foreach (var role in roles)
+			{
+				if (!await roleManager.RoleExistsAsync(role)) {
+					await roleManager.CreateAsync(new IdentityRole(role));
+				}
+			}
+
+			var adminEmail = "admin@OnlineTutor.com";
+			var adminUser = await userManager.FindByNameAsync(adminEmail);
+
+			if (adminUser == null)
+			{
+				var newAdmin = new IdentityUser
+				{
+					UserName = adminEmail,
+					Email = adminEmail,
+					EmailConfirmed = true
+				};
+
+				await userManager.CreateAsync(newAdmin, "Str0ngPassw0rd!");
+				await userManager.AddToRoleAsync(newAdmin, "Admin");
+			}
+		}
+	}
+}
